0b7eab7f60a5803da992f41fd6fd257f9b02b9d5
biabia2 <biabia2@hotmail.com>
2022-11-04 22:09:43 -0300

    Alteração nas informações do banco de dados

--- glossario.php
+++ glossario.php
@@ -2,31 +2,32 @@
 
 <section>
 
-    <a href="#"> A </a>
-    <a href="#"> B </a>
-    <a href="#"> C </a>
-    <a href="#"> E </a>
-    <a href="#"> F </a>
-    <a href="#"> G </a>
-    <a href="#"> G </a>
-    <a href="#"> I </a>
-    <a href="#"> J </a>
-    <a href="#"> K </a>
-    <a href="#"> L </a>
-    <a href="#"> M </a>
-    <a href="#"> N </a>
-    <a href="#"> O </a>
-    <a href="#"> P </a>
-    <a href="#"> Q </a>
-    <a href="#"> R </a>
-    <a href="#"> S </a>
-    <a href="#"> T </a>
-    <a href="#"> U </a>
-    <a href="#"> V </a>
-    <a href="#"> W </a>
-    <a href="#"> X </a>
-    <a href="#"> Y </a>
-    <a href="#"> Z </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=A"> A </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=B"> B </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=C"> C </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=D"> D </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=E"> E </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=F"> F </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=G"> G </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=H"> H </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=I"> I </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=J"> J </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=K"> K </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=L"> L </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=M"> M </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=N"> N </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=O"> O </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=P"> P </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=Q"> Q </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=R"> R </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=S"> S </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=T"> T </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=U"> U </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=V"> V </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=W"> W </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=X"> X </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=Y"> Y </a>
+    <a href="painel-administrativo/consulta-glossario.php?letra=Z"> Z </a>
 
 </section>
 
--- painel-administrativo/consulta-glossario.php
+++ painel-administrativo/consulta-glossario.php
@@ -1,12 +1,21 @@
+<?php
+
+	$letrarecebida = $_GET ["letra"];
+	echo $letrarecebida;
+
+
+?>
+
 <?php include('painel-administrativo/rodape.php'); ?>
 
 <?php 
 	include("conexao.php");
 
-	$stmt = $pdo->prepare("select * from tbPalavra");	
+	$stmt = $pdo->prepare("select * from tbPalavra where palavra like '$letter%'");	
 	$stmt ->execute();
 	
 	while($row = $stmt ->fetch(PDO::FETCH_BOTH)){	
+
 		echo "<p> $row[idPalavra] $row[1] $row[2] $row[3] $row[4] </p>" ;												
 	}
 ?>
